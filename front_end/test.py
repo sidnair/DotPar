12979e022433e32d13895a73c0b80663446f8537
Nathan Hwang <beta.entity.k@gmail.com>
2012-03-21 00:27:12 -0400

    Made the test suite functional

--- front_end/test.py
+++ front_end/test.py
@@ -1,87 +1,252 @@
 #!/usr/bin/python
 
 import subprocess
+import time
 
 tests = [
-# check the one liners
+####################
+# anti-check simple statements
 ("""1+1""", False),
 ("""aoeu""", False),
 ("""number x = 0""", False),
-
 ("""1+1;""", False),
 ("""aoeu;""", False),
 ("""number x = 0;""", False),
-
-# check the one liner basics wrapped in main
-("""
-func main:number() {
-}""", True),
-("""
-func main:number() {}""", True), # check whitespace
-("""
-func main:number() {
-  1+1;
-}""", True),
-("""
-func main:number() {
-  aoeu;
-}""", True),
-("""
-func main:number() {
-  number a = 1+a;
-}""", True),
-("""
-func main:number(char x) {
-  number a = 1+a;
-}""", True),
-("""
-func main:number(char x) {
-  number a = 1+a;
-}""", True),
-
-# check the multiple lines
-("""
-func main:number(char x) {
-  number a = 1+a;
-  number b;
-}""", True),
-
-# check array syntax
-("""
-func main:number(char x) {
-  number[] a = b;
-}""", True),
-("""
-func main:number(char x) {
-  char[][][][] a = b;
-}""", True),
-
-# check function syntax
-("""
-func main:void(char x) {
-  func:void() b;
-}""", True),
-("""
-func main:void(char x) {
-  func:number[] (func:number(), func:char()) b;
-}""", True),
-("""
-func main:void(char x) {
-  func:number[] (func:number(), func:char()) b;
-}""", True),
-
-# anti-check simple statements
+####################
+# simple statements
+("""func main:void() { ; }""", True),
+####################
+# check simple types
+("""func main:void(char x) { }""", True),
+("""func main:void(number x) { }""", True),
+("""func main:void(boolean x) { }""", True),
+("""func main:void() { char x; }""", True),
+("""func main:void() { number x; }""", True),
+("""func main:void() { boolean x; }""", True),
+####################
 # check arithmetic expressions
-# check strings
-
-# check array expressions (also, append)
-# check func
+("""
+func main:void() { 2+2; }""", True),
+("""
+func main:void() {
+  (2*2-5.0)/(.988999+3.0%9);
+}""", True),
+####################
+# check boolean expressions
+("""func main:void() { true; }""", True),
+("""func main:void() { false; }""", True),
+("""
+func main:void() {
+  true && false;
+}""", True),
+("""
+func main:void() {
+  true && not (false || true && (false || !true));
+}""", True),
+####################
+# check comparisons
+("""func main:void() { true == x; }""", True),
+("""func main:void() { x*y > y/19.08; }""", True),
+("""func main:void() { y/19.08 < x*y; }""", True),
+("""func main:void() { 10.05/40.5 >= x+y; }""", True),
+("""func main:void() { x <= y; }""", True),
+####################
+# check char literals
+("""func main:void() { 'a'; }""", True),
+(r"""func main:void() { '\n'; }""", True),
+(r"""func main:void() { '\'; }""", True),
+("""func main:void() { char x = 'b'; }""", True),
+# anti-check
+("""func main:void() { ''; }""", False),
+####################
+# check strings literals
+("""func main:void() { ""; }""", True),
+("""func main:void() { "a"; }""", True),
+("""func main:void() { "a"; }""", True),
+("""func main:void() { "hello world 29.0"; }""", True),
+(r"""func main:void() { "\"\n\\"; }""", True),
+# anti-check
+("""func main:void() { "a"b"; }""", False),
+####################
+# check array types
+("""func main:void(char[] x) { }""", True),
+("""func main:void(number[] x) { }""", True),
+("""func main:void(boolean[] x) { }""", True),
+("""func main:void(char[][][] x) { }""", True),
+("""func main:void() { char[][] x; }""", True),
+####################
+# check array literals
+("""func main:void() { [1, 2, 3.1]; }""", True),
+("""func main:void() { ['a','b']; }""", True),
+("""func main:void() { ["hello","world"]; }""", True),
+("""func main:void() { [true, false]; }""", True),
+####################
+# check array indexing/slicing
+("""func main:void() { a[0]; }""", True),
+("""func main:void() { a[x]; }""", True),
+("""func main:void() { a[x+5]; }""", True),
+("""func main:void() { a[f()]; }""", True),
+("""func main:void() { f()[1]; }""", True),
+("""func main:void() { f()[g()]; }""", True),
+####################
+# check array expressions
+("""func main:void() { number[] x = [10, 20.0]; }""", True),
+("""func main:void() { x[10] = [10.0, 20]; }""", True),
+####################
+# func types
+("""func main:void() {
+  func:void() x;
+}""", True),
+("""func main:void() {
+  func:void(number a) x = b;
+}""", True),
+("""func main:void() {
+  func:void(func:void() documentation) x;
+}""", True),
+####################
+# func calls
+("""func main:void() { f(); }""", True),
+("""func main:void() { f(x); }""", True),
+("""func main:void() { f(x, y); }""", True),
+("""func main:void() { f(10.0, 5.0); }""", True),
+("""func main:void() { f("thing", "mu"); }""", True),
+("""func main:void() { f('1', "thing", 105.24); }""", True),
+("""func main:void() { f(g(h())); }""", True),
+# returns a function, which we call again
+("""func main:void() { f(1)(2)(3); }""", True),
+####################
+# func definitions (normal)
+("""func main:void() {
+  func foo:number() {
+    ;
+  }
+}""", True),
+("""func main:void() {
+  func foo:number() {
+    return 2+2;
+  }
+}""", True),
+("""func main:void() {
+  func foo:void() {
+    func bar:void() {
+    }
+  }
+}""", True),
+####################
+# func definitions (anonymous)
+("""func main:void() {
+  func:void() x = func:void() { };
+}""", True),
+("""func main:void() {
+  func:number(char[]) x = func:number(char[] s) {
+    return 2;
+  };
+}""", True),
+####################
 # if
-# for
+("""func main:void() {
+  if(something) {
+    do_something();
+  }
+}""", True),
+("""func main:void() {
+  if(something) {
+    do_something();
+  } else {
+    do_something_else();
+  }
+}""", True),
+("""func main:void() {
+  if(something) {
+    do_something();
+  } elif(something_else) {
+    do_something_else();
+  } else {
+    do_the_other_thing();
+  }
+}""", True),
+("""func main:void() {
+  if(something) {
+    if(and_something) {
+     do_something();
+    }
+  }
+}""", True),
+####################
+# normal for
+("""func main:void() {
+  for(;;) {
+  }
+}""", True),
+("""func main:void() {
+  for(x = 10; x < y; x = x +1) {
+    do_something();
+  }
+}""", True),
+("""func main:void() {
+  for(number x = 10; x < y; x = x + 1) {
+    do_something_else();
+  }
+}""", True),
+####################
 # list comprehensions
-# 
+("""func main:void() {
+  [1 for number x in y];
+}""", True),
+("""func main:void() {
+  [1 for number x in [1,2,3]];
+}""", True),
+("""func main:void() {
+  x = [1 for (number x) in c];
+}""", True),
+("""func main:void() {
+  x[1] = [1 for number x, number y in [1,2], [3,4]];
+}""", True),
+("""func main:void() {
+  x[1] = [1 for number x, number y in ([1,2], [3,4])];
+}""", True),
+("""func main:void() {
+  x[1] = [1 for (number x, number y) in ([1,2], [3,4])];
+}""", True),
+("""func main:void() {
+  return [[x*y for number y in bb] for number x in aa];
+}""", True),
+####################
+# imports
+("""import math;""", False),
+("""import math; number a;""", True),
+("""import math; import types; number a;""", True),
+####################
+# foreach
+####################
+# some examples
 ]
+
+test_errors = 0
 
 for test in tests:
     # run each test, check the return value
-    pass
+    proc = subprocess.Popen(["./a.out"],
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    proc.communicate(test[0]+"\n")
+    # wait a bit
+    time.sleep(0.01)
+    # poll
+    status = proc.poll()
+    # end it if it's still running
+    if status is None:
+        proc.terminate()
+    # check if we're
+    if (status is None) == test[1] or (status == 0) == test[1]:
+        print("test okay")
+    else:
+        print("")
+        print(status is None, test[1])
+        print("test failed:")
+        print(test[0])
+        print("")
+        test_errors += 1
+
+print("total failures: %d/%d" % (test_errors, len(tests)) )
