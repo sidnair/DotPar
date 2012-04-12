daaa57abae250579e1bca80a78250d0be2332611
Nathan Hwang <beta.entity.k@gmail.com>
2012-04-11 22:24:18 -0400

    Changed dotpar toplevel to output the program after AST
    Also, added comments to mark where semantic analysis and
    code generation steps should happen

--- src/ocaml/dotpar.ml
+++ src/ocaml/dotpar.ml
@@ -1,8 +1,12 @@
+open Ast
+open Printf
+
 let _ =
   let lexbuf = Lexing.from_channel stdin in 
-  try
+  let ast =
+    try
       Parser.program Scanner.token lexbuf
-  with
+    with
     | Failure(s)             -> prerr_endline ("Error"); exit 1
     | Parsing.Parse_error    ->
       let p = Lexing.lexeme_start_p lexbuf in
@@ -10,3 +14,11 @@ let _ =
       let c = p.Lexing.pos_cnum - p.Lexing.pos_bol + 1 in
       Printf.fprintf stderr "Parsing error at line %d, char %d\n." l c;
       exit 1
+  in
+  (* DEBUG: reprint the AST *)
+  Printf.printf "%s" (string_of_program ast);
+  (* Semantic analysis *)
+
+  (* Code generation *)
+
+;;
